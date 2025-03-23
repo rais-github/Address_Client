7ae6034a8f575d36efc3449d57c37d6270ceee79
Himanshu Rai <himanshu2003rai@gmail.com>
2025-03-23 10:20:31 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'address-book-client';
+  persons: any[] = [];
+  isModalOpen = false;
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
 }
